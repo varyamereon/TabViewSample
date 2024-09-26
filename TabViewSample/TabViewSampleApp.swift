3c543b42d3ca252b1834c194da5652a325c4e184
Will Beal <willbeal@spreefortythree.com>
2024-09-26 12:24:11 +0200

    Initial Commit

---
+++ TabViewSample/TabViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewSampleApp.swift
+//  TabViewSample
+//
+//  Created by Will Beal on 26.09.24.
+//
+
+import SwiftUI
+
+@main
+struct TabViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
